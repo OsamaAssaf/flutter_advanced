1b21a688dec402821d6c319d90bac273c0fdfae7
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2024-03-26 13:41:05 +0300

    commit

--- lib/modules/get_widget/view/get_widget_view.dart
+++ lib/modules/get_widget/view/get_widget_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/components/accordion/gf_accordion.dart';
+import 'package:getwidget/components/alert/gf_alert.dart';
+import 'package:getwidget/components/appbar/gf_appbar.dart';
 import 'package:getwidget/components/avatar/gf_avatar.dart';
+import 'package:getwidget/getwidget.dart';
 
 class GetWidgetView extends StatelessWidget {
   const GetWidgetView({super.key});
@@ -7,13 +11,56 @@ class GetWidgetView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: GFAppBar(
         title: const Text('Get Widget'),
       ),
-      body: Column(
-        children: [
-          GFAvatar(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            GFSearchBar(
+              searchList: const [
+                'One',
+                'Two',
+                'Three',
+                'Four',
+              ],
+              overlaySearchListItemBuilder: (String? value) {
+                return Text(value!);
+              },
+              searchQueryBuilder: (String? value, List<String> values) {
+                return values;
+              },
+            ),
+            const GFAccordion(
+              title: 'Title',
+              content: 'Content',
+            ),
+            const GFAlert(),
+            const GFAppBarTitleBar(
+              child: Text('GFAppBarTitleBar'),
+            ),
+            GFButton(
+              child: const Text('Button'),
+              onPressed: () {},
+            ),
+            GFButtonBadge(
+              onPressed: () {},
+              text: 'GFButtonBadge',
+            ),
+            const GFLoader(
+              type: GFLoaderType.ios,
+            ),
+            GFMultiSelect(
+              items: const [
+                'One',
+                'Two',
+                'Three',
+                'Four',
+              ],
+              onSelect: (values) {},
+            ),
+          ],
+        ),
       ),
     );
   }
