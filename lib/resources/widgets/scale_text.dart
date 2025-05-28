d91601e8cc02da40ef4c4969fc90a04fc610997f
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2025-05-28 08:54:32 +0300

    commit

--- lib/resources/widgets/scale_text.dart
+++ lib/resources/widgets/scale_text.dart
@@ -25,7 +25,7 @@ class ScaleText extends StatelessWidget {
 mixin ScaleSize {
   static double textScaleFactor(BuildContext context,
       {double maxTextScaleFactor = 2}) {
-    final width = MediaQuery.of(context).size.width;
+    final double width = MediaQuery.of(context).size.width;
     final double val = (width / 1400) * maxTextScaleFactor;
     return max(1, min(val, maxTextScaleFactor));
   }
